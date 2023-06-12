ba22443cdc3343cc79f3ecb2c82b649abf9dcbe5
Kazi Arham <210149902@aston.ac.uk>
2023-06-12 22:18:18 +0100

    Rendering data dynamically

--- script.js
+++ script.js
@@ -25,10 +25,46 @@ let myData = [
         fullname: function () {
             return this.first_name + " " + this.last_name;
         }
+    },
+
+    {
+        first_name: "John", 
+        last_name: "Smith", 
+        age: 37,
+        fullname: function () {
+            return this.first_name + " " + this.last_name;
+        }
+    },
+
+    {
+        first_name: "James", 
+        last_name: "Anderson", 
+        age: 31,
+        fullname: function () {
+            return this.first_name + " " + this.last_name;
+        }
     }
 ];
 
-console.log(myData[0].first_name);
-console.log(myData[0].last_name);
-console.log(myData[0].age);
-console.log(myData[0].fullname());
+// Creating HTML elements for objects
+let listHolder = document.querySelector("#listHolder");
+
+// Function to render data dynamically
+function renderRecord() {
+    // For each loop to loop through the data as an item
+    myData.forEach((item, index) => {
+        // Creates a tr element
+        let tr = document.createElement("tr");
+
+        // Adds data to the table
+        tr.innerHTML = 
+            `<td>${index + 1}</td>
+            <td>${item.fullname()}</td>
+            <td>${item.age}</td>`;
+
+        // It is appended to the table using the listHolder ID
+        listHolder.appendChild(tr);
+    });
+}
+
+renderRecord();
