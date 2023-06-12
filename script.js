672c89a375dcaec0d5e7042ae4230f7f6d7048b3
Kazi Arham <210149902@aston.ac.uk>
2023-06-12 15:07:18 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// Array containing objects
+let myData = [
+    {
+        first_name: "Paul", 
+        last_name: "Lucero", 
+        age: 32,
+        fullname: function () {
+            return this.first_name + " " + this.last_name;
+        }
+    },
+
+    {
+        first_name: "Kasey", 
+        last_name: "Payne", 
+        age: 33,
+        fullname: function () {
+            return this.first_name + " " + this.last_name;
+        }
+    },
+
+    {
+        first_name: "Lakeisha", 
+        last_name: "Rivas", 
+        age: 27,
+        fullname: function () {
+            return this.first_name + " " + this.last_name;
+        }
+    }
+];
+
+console.log(myData[0].first_name);
+console.log(myData[0].last_name);
+console.log(myData[0].age);
+console.log(myData[0].fullname());
